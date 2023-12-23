55f95b09a48469eb8dcd80dd7e80df61fd7aca2d
cF-embedded <filippocerta3@gmail.com>
2023-12-23 19:37:05 +0100

    stack_tests: 1 test

--- test/stack_tests/test.cpp
+++ test/stack_tests/test.cpp
@@ -5,4 +5,13 @@ extern "C" {
 #include <gtest/gtest.h>
 
 
+TEST(StackTests, stack_is_empty_after_init) 
+{
+    stack_s_t stack;
+
+    stack_init(&stack);
+
+    EXPECT_TRUE(stack_is_empty(&stack));
+}
+
 

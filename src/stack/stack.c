904a92a55e8e5aeefaacfd8020d1e38d4338bb66
cF-embedded <filippocerta3@gmail.com>
2023-12-23 19:37:18 +0100

    stack: 1 test implementation

---
+++ src/stack/stack.c
@@ -0,0 +1,11 @@
+#include "stack.h"
+
+void stack_init(stack_s_t * stack)
+{
+
+}
+
+bool stack_is_empty(stack_s_t * stack)
+{
+    return true;
+}
--- src/stack/stack.h
+++ src/stack/stack.h
@@ -1,3 +1,14 @@
 #pragma once
 
-#include "stdint.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct 
+{
+    uint32_t buffer[32];
+    uint8_t top;
+} stack_s_t; 
+
+void stack_init(stack_s_t *);
+
+bool stack_is_empty(stack_s_t *);

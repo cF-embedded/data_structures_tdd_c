28893ffe8ecaf6c56a15ded2a3f038499e96cf5e
cF-embedded <filippocerta3@gmail.com>
2023-12-23 20:05:41 +0100

    stack_tests: 4 test

--- test/stack_tests/test.cpp
+++ test/stack_tests/test.cpp
@@ -5,7 +5,7 @@ extern "C" {
 #include <gtest/gtest.h>
 
 
-TEST(StackTests, stack_is_empty_after_init) 
+TEST(StackTests, is_empty_after_init) 
 {
     stack_s_t stack;
 
@@ -14,7 +14,7 @@ TEST(StackTests, stack_is_empty_after_init)
     EXPECT_TRUE(stack_is_empty(&stack));
 }
 
-TEST(StackTests, stack_not_empty_after_adding_item)
+TEST(StackTests, not_empty_after_adding_item)
 {
     stack_s_t stack;
     item_t item = 5;
@@ -23,6 +23,17 @@ TEST(StackTests, stack_not_empty_after_adding_item)
     stack_push(&stack, item);
 
     EXPECT_FALSE(stack_is_empty(&stack));
+}
+
+TEST(StackTests, added_item_is_equal_item_on_top)
+{
+    stack_s_t stack;
+    item_t item = 13;
+
+    stack_init(&stack);
+    stack_push(&stack, item);
+
+    EXPECT_EQ(item, stack_peek(&stack));
 }
 
 

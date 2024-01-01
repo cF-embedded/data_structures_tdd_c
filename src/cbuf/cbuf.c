9199e64008e4806bce27b70072bb00a3a541af60
cF-embedded <filippocerta3@gmail.com>
2024-01-01 19:42:23 +0100

    cbuf: 1 test implementation

--- src/cbuf/cbuf.c
+++ src/cbuf/cbuf.c
@@ -1,2 +1,10 @@
 #include "cbuf.h"
 
+void cbuf_init(cbuf_s_t *)
+{
+}
+
+bool cbuf_is_empty(cbuf_s_t *)
+{
+    return true;
+}
--- src/cbuf/cbuf.h
+++ src/cbuf/cbuf.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#define CBUF_SIZE 32
+
+typedef uint32_t buffer_t;
+typedef size_t index_t;
+
+/* if buffer size changes, item must also change */
+typedef buffer_t item_t;
+
+typedef struct 
+{
+    buffer_t buffer[CBUF_SIZE];
+    index_t head;
+    index_t tail;
+} cbuf_s_t; 
+
+void cbuf_init(cbuf_s_t *);
+
+bool cbuf_is_empty(cbuf_s_t *);

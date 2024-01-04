e6f68caddf78eee5145fcffd2200be349bfa958c
cF-embedded <filippocerta3@gmail.com>
2024-01-04 20:43:15 +0100

    cbuf_tests: 4 test

--- test/cbuf_tests/test.cpp
+++ test/cbuf_tests/test.cpp
@@ -33,6 +33,20 @@ TEST(CbufTests, not_empty_after_added_item)
     EXPECT_FALSE(cbuf_is_empty(&cbuf));
 }
 
+TEST(CbufTests, tail_increment_after_added_item)
+{
+    cbuf_s_t cbuf;
+    item_t item = 0xFFFF;
+    index_t last_tail_val;
+
+    cbuf_init(&cbuf);
+    last_tail_val = cbuf_get_tail(&cbuf);
+
+    cbuf_push(&cbuf, item);
+    last_tail_val++;
+    EXPECT_EQ(last_tail_val, cbuf_get_tail(&cbuf));
+}
+
 
 
 

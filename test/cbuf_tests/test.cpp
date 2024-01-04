65961946690cdfc03e6d1edd4dcd1ba98fbdf20c
cF-embedded <filippocerta3@gmail.com>
2024-01-04 20:29:33 +0100

    cbuf: test 2 refactor

--- test/cbuf_tests/test.cpp
+++ test/cbuf_tests/test.cpp
@@ -4,7 +4,7 @@ extern "C" {
 
 #include <gtest/gtest.h>
 
-TEST(StackTests, is_empty_after_init) 
+TEST(CbufTests, is_empty_after_init) 
 {
     cbuf_s_t cbuf;
 
@@ -13,13 +13,15 @@ TEST(StackTests, is_empty_after_init)
     EXPECT_TRUE(cbuf_is_empty(&cbuf));
 }
 
-TEST(StackTests, head_equal_tail_after_init)
+TEST(CbufTests, head_equal_tail_after_init)
 {
     cbuf_s_t cbuf;
 
-    cbuf_init(&cbuf)
+    cbuf_init(&cbuf);
 
     EXPECT_TRUE(cbuf_get_head(&cbuf) == cbuf_get_tail(&cbuf));
 }
+
+
 
 

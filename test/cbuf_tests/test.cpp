d467bafdb49ada0b770c4d00bac88f9f99be61c2
cF-embedded <filippocerta3@gmail.com>
2024-01-04 19:58:21 +0100

    cbuf_tests: 2 test

--- test/cbuf_tests/test.cpp
+++ test/cbuf_tests/test.cpp
@@ -13,4 +13,13 @@ TEST(StackTests, is_empty_after_init)
     EXPECT_TRUE(cbuf_is_empty(&cbuf));
 }
 
+TEST(StackTests, head_equal_tail_after_init)
+{
+    cbuf_s_t cbuf;
+
+    cbuf_init(&cbuf)
+
+    EXPECT_TRUE(cbuf_get_head(&cbuf) == cbuf_get_tail(&cbuf));
+}
+
 
